d0d3cf3f57e47cd47f3643c6c420b5a9a703ebdd
sjs08 <sjs08@DESKTOP-U8QUNSQ>
2022-03-09 20:55:20 +0900

    2중for문을 이용한 구구단

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -1,54 +1,10 @@
 package gugudan;
 
-import java.util.Scanner;
-
-public class Gugudan {
+public class Gugudan{
 	public static void main(String[] args) {
-		
-//		  // 2단 // System.out.println("2단"); // System.out.println(2 * 1); //
-//		  System.out.println(2 * 2); // System.out.println(2 * 3); //
-//		  System.out.println(2 * 4); // System.out.println(2 * 5); //
-//		  System.out.println(2 * 6); // System.out.println(2 * 7); //
-//		  System.out.println(2 * 8); // System.out.println(2 * 9); // // // 3단 //
-//		  System.out.println("3단"); // System.out.println(3 * 1); //
-//		  System.out.println(3 * 2); // System.out.println(3 * 3); //
-//		  System.out.println(3 * 4); // System.out.println(3 * 5); //
-//		  System.out.println(3 * 6); // System.out.println(3 * 7); //
-//		  System.out.println(3 * 8); // System.out.println(3 * 9);
-//		  
-//		  //4단 // System.out.println("4단"); // int result = 4 * 1; //
-//		  System.out.println(result); // result = 4 * 3; //
-//		  System.out.println(result); // result = 4 * 4; //
-//		  System.out.println(result); // result = 4 * 5; //
-//		  System.out.println(result); // result = 4 * 6; //
-//		  System.out.println(result); // result = 4 * 7; //
-//		  System.out.println(result); // result = 4 * 8; //
-//		  System.out.println(result); // result = 4 * 9; //
-//		  System.out.println(result);
-		 		
-
-/*
- * System.out.println("구구단 중 출력할 단은? : "); Scanner scanner = new
- * Scanner(System.in); int number = scanner.nextInt(); System.out.println(number
- * 1); System.out.println(number * 2); System.out.println(number * 3);
- * System.out.println(number * 4); System.out.println(number * 5);
- * System.out.println(number * 6); System.out.println(number * 7);
- * System.out.println(number * 8); System.out.println(number * 9);
- */
-		System.out.println("몇 단을 출력하시겠습니까? : ");
-		Scanner scanner = new Scanner(System.in);
-		int dan = scanner.nextInt();
-		System.out.println("사용자가 입력한 값: " + dan);
-		int i = 1;
-		
-		while (i <= 9) {
-			if (dan < 2 || dan > 9) {
-				System.out.println("잘못된 값을 입력하였습니다. 2~9까지의 숫자를 입력하세요.");
-				break;
-			}
-			else {
-				System.out.println(dan * i);
-				i += 1;
+		for (int dan = 1; dan <= 9; dan++) {
+			for (int i = 1; i <= 9; i++) {
+				System.out.println(dan*i);
 			}
 		}
 	}
