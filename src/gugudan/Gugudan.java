9815ea3ee09968f72b7a2b07ff174035b4a9efcf
sjs08 <sjs08@DESKTOP-U8QUNSQ>
2022-03-09 21:05:50 +0900

    계산과정까지 출력하기

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -3,8 +3,8 @@ package gugudan;
 public class Gugudan{
 	public static void main(String[] args) {
 		for (int dan = 1; dan <= 9; dan++) {
-			for (int i = 1; i <= 9; i++) {
-				System.out.println(dan*i);
+			for (int times = 1; times <= 9; times++) {
+				System.out.println(Integer.toString(dan) + "X" + Integer.toString(times) + "=" + Integer.toString(dan*times));
 			}
 		}
 	}
