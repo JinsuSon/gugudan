dc38e0dc12e03af14ab8f7425522a5cbfa228e58
JinsuSon <sjs0882@naver.com>
2022-03-27 16:18:07 +0900

    메서드를 이용한 구구단프로그램

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -1,11 +1,27 @@
 package gugudan;
 
 public class Gugudan{
-	public static void main(String[] args) {
-		for (int dan = 1; dan <= 9; dan++) {
-			for (int times = 1; times <= 9; times++) {
-				System.out.println(Integer.toString(dan) + "X" + Integer.toString(times) + "=" + Integer.toString(dan*times));
-			}
+	public static int[] calculate(int times) {
+		int [] result = new int[9];
+		for(int i=0; i < result.length; i++) {
+			result[i] = times * (i + 1);
 		}
+		return result;
+	}
+	public static void print(int[] result) {
+		for(int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		int [] result = calculate(2);
+		print(result);
+		
+		int[] times3 = calculate(3);
+		print(times3);
+		
+		int[] times4 = calculate(4);
+		print(times4);
 	}
 }
